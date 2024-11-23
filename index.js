f62b86985c8a6e0ad5f44bf88b8367fe073b2342
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-11-23 23:58:37 +0400

    bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const HOST =
-  location.hostname !== "socket-psli.onrender.com"
+  location.hostname !== "sofg1.github.io"
     ? "192.168.100.84:3000"
     : "https://socket-psli.onrender.com/";
 
