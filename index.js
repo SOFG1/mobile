5ce3a9663ab755eeaaf86e74f8c1dfdf224447f0
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-11-23 23:55:23 +0400

    qwdqdwq

---
+++ index.js
@@ -0,0 +1,101 @@
+const HOST =
+  location.hostname !== "socket-psli.onrender.com"
+    ? "192.168.100.84:3000"
+    : "https://socket-psli.onrender.com/";
+
+const socket = io(HOST);
+window.socket = socket;
+
+let session = false;
+let interval;
+
+window.settings = {
+  speed: "3000",
+};
+
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+
+const input = document.querySelector(".number");
+
+startBtn.addEventListener("click", (e) => {
+  socket.emit("session", "start");
+});
+
+stopBtn.addEventListener("click", (e) => {
+  socket.emit("session", "stop");
+});
+
+socket.on("session", (arg) => {
+  if (arg === "start") {
+    startBtn.setAttribute("disabled", true);
+    stopBtn?.removeAttribute("disabled");
+    window.start();
+  }
+  if (arg === "stop") {
+    if (!session) return;
+    session = false;
+    document.querySelector(".started")?.remove();
+    const el = document.createElement("div");
+    el.classList.add("circle");
+    el.classList.add("static");
+    document.body.appendChild(el);
+    startBtn?.removeAttribute("disabled");
+    stopBtn?.setAttribute("disabled", true);
+    clearInterval(interval);
+  }
+});
+
+//Settings
+input?.addEventListener("change", (e) => {
+  const regex = /^\d+$/;
+  const val = e.target.value;
+  if (!regex.test(val)) {
+    e.target.value = settings.speed;
+    return;
+  }
+  const num = Number(val);
+  if (num < 3000 || num > 8000) {
+    e.target.value = settings.speed;
+    return;
+  }
+  settings.speed = e.target.value;
+  socket.emit("settings", settings);
+});
+
+socket.on("settings", (arg) => {
+  settings = arg;
+  // input.value = settings.speed;
+});
+
+window.start = function start() {
+  if (session) return;
+  session = true;
+  //oneCicle();
+  // interval = setInterval(oneCicle, Number(settings.speed));
+};
+
+function oneCicle() {
+  document.querySelector(".circle")?.remove(); //Remove static
+  const el = document.createElement("div");
+  el.classList.add("circle");
+  el.classList.add("started");
+  document.body.appendChild(el);
+  const duration = window.settings.speed;
+  el.style.animationDuration = `${duration}ms`;
+  el.classList.add("started");
+  setTimeout(playAudio, duration / 2);
+  setTimeout(playAudio, duration);
+}
+
+window.oneCicle = oneCicle;
+
+function playAudio() {
+  if (!document.querySelector(".started")) return;
+  const audio = new Audio("1.wav");
+  audio.play();
+}
+
+socket.on("connect", () => {
+  socket.emit("device", "mobile");
+});
---
+++ webrtc.js
@@ -0,0 +1,57 @@
+let localConnection;
+let remoteConnection;
+let sendChannel;
+let receiveChannel;
+
+socket.on("offer", handleOffer);
+socket.on("answer", handleAnswer);
+socket.on("ice-candidate", handleIceCandidate);
+
+function handleOffer(offer) {
+  remoteConnection = new RTCPeerConnection();
+  remoteConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      socket.emit("ice-candidate", event.candidate);
+    }
+  };
+
+  remoteConnection
+    .setRemoteDescription(new RTCSessionDescription(offer))
+    .then(() => {
+      return remoteConnection.createAnswer();
+    })
+    .then((answer) => {
+      remoteConnection.setLocalDescription(answer);
+      socket.emit("answer", answer);
+    })
+    .catch((error) => console.error("Error handling offer: ", error));
+
+  remoteConnection.ondatachannel = (event) => {
+    receiveChannel = event.channel;
+    receiveChannel.onmessage = (event) => {
+      const message = event.data;
+      //alert(message);
+      oneCicle();
+      console.log(message);
+    };
+  };
+}
+
+function handleAnswer(answer) {
+  localConnection
+    .setRemoteDescription(new RTCSessionDescription(answer))
+    .catch((error) => console.error("Error handling answer: ", error));
+}
+
+function handleIceCandidate(candidate) {
+  const iceCandidate = new RTCIceCandidate(candidate);
+  if (localConnection) {
+    localConnection
+      .addIceCandidate(iceCandidate)
+      .catch((error) => console.error("Error adding ICE candidate: ", error));
+  } else {
+    remoteConnection
+      .addIceCandidate(iceCandidate)
+      .catch((error) => console.error("Error adding ICE candidate: ", error));
+  }
+}
